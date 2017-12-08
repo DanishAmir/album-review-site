a45e018b628b2c70185cae6692e2c4ff7e5b39a6
Danish Amir <danish.amir1997@gmail.com>
2017-12-08 17:21:14 +0000

    Rendered some more views

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,2 +1,4 @@
 class Album < ActiveRecord::Base
+  validates :year, numericality: { less_than_or_equal_to: 2017 } ##cannot review album which hasn't been released
+  validates :year, numericality: { more_than_or_equal_to: 1947 } ##first studio album 
 end
