e65a07c0b57f2ae2a775cc254095ca95a2f481ec
Danish Amir <danish.amir1997@gmail.com>
2017-12-09 13:44:51 +0000

    User can only edit their album entry

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,4 +1,5 @@
 class Album < ActiveRecord::Base
+    belongs_to :user
   validates :year, numericality: { less_than_or_equal_to: 2017 } ##cannot review album which hasn't been released
   validates :year, numericality: { more_than_or_equal_to: 1947 } ##first studio album 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171209124517) do
+ActiveRecord::Schema.define(version: 20171209130758) do
 
   create_table "albums", force: :cascade do |t|
     t.string   "title",       null: false
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20171209124517) do
     t.integer  "year",        null: false
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
